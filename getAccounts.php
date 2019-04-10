92db05c1541df9b7c078b67866e8be577e633cb9
henrydatei <henrydatei@web.de>
2019-04-10 17:38:39 +0100

    Create getAccounts.php

---
+++ getAccounts.php
@@ -0,0 +1,12 @@
+<?php
+include('dbconnect.php');
+$sql = "SELECT * FROM `accounts`";
+$back = mysqli_query($db, $sql);
+while ($row = mysqli_fetch_array($back)) {
+  $username = $row["username"];
+  $password = $row["password"];
+  $alt = $row["displayed_name"];
+  print "$username:$password:$alt";
+  print "<br>";
+}
+ ?>
