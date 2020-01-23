89e0ca5de35b62b4b1fb628a70a9fe0f1d0eeb3b
henrydatei <henrydatei@web.de>
2020-01-23 12:08:18 +0100

    fixed problem with inserting in DB

--- newAlt/newalt.php
+++ newAlt/newalt.php
@@ -3,10 +3,10 @@ include("../dbconnect.php");
 include("../create_arrays.php");
 
 //Accounts-Datenbank
-$zugangsdaten= htmlentities($_GET['LoginData'], ENT_QUOTES);
+$zugangsdaten = $_GET['LoginData'];
 $zugangsdaten = mysqli_real_escape_string($db, $zugangsdaten);
 $userpass = explode(':',$zugangsdaten);
-$neueralt = htmlentities($_GET['neueralt'], ENT_QUOTES);
+$neueralt = $_GET['neueralt'];
 $neueralt = mysqli_real_escape_string($db, $neueralt);
 $adddb= "INSERT INTO `accounts`(`username`, `password`, `displayed_name`, `id`) VALUES ('$userpass[0]','$userpass[1]','$neueralt','')";
 $einfugen = mysqli_query($db, $adddb);
@@ -20,10 +20,8 @@ $spalteres = mysqli_query($db, "ALTER TABLE `alts` ADD `$neueralt` TEXT NULL AFT
 // Eintrag in history-Datenbank machen
 $datum = date("d.m.Y", time());
 $uhrzeit = date("H:i", time());
-$ip = htmlentities($_SERVER['REMOTE_ADDR'], ENT_QUOTES);
-$ip = mysqli_real_escape_string($db, $ip);
-$useragent = htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES);
-$useragent = mysqli_real_escape_string($db, $useragent);
+$ip = $_SERVER['REMOTE_ADDR'];
+$useragent = $_SERVER['HTTP_USER_AGENT'];
 $account = "Webseite/API";
 $action = "Alt hinzu";
 $sql = "INSERT INTO `history` (`id`, `datum`, `uhrzeit`, `ip`, `useragent`, `account`, `action`, `alt`, `main`, `bann_vorher`, `bann_nachher`, `server`) VALUES (NULL, '$datum', '$uhrzeit', '$ip', '$useragent', '$account', '$action', '$neueralt', NULL, NULL, NULL, NULL)";
@@ -33,6 +31,9 @@ $f=fopen('../lastupdate.txt','w');
 $time = time();
 fwrite($f,"$time");
 fclose($f);
+
+//print "$adddb<br/>";
+//print "$sql";
 
 header("Location: ../");
 ?>
